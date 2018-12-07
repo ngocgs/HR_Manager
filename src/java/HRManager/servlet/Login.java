60d4f41d71d5f43cc5c52838d2ba1f8cb50ec5d0
ngocgs <32269465+ngocgs@users.noreply.github.com>
2018-12-07 23:14:15 +0700

    Add files via upload

---
+++ src/java/HRManager/servlet/Login.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HRManager.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import HRManager.entities.User;
+import HRManager.bol.UserBO;
+import javax.servlet.RequestDispatcher;
+
+public class Login extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String userName = request.getParameter("txtUserName");
+        String userPassword = request.getParameter("txtPassword");
+        System.out.println("username"+userName);
+        System.out.println("userPassword"+userPassword);
+        
+        HRManager.ValidData valid = new HRManager.ValidData();
+        if (userName.length()==0) {
+            goPage(request, response, "login.jsp?error=UserName");
+        } else if (userPassword.length()==0) {
+            goPage(request, response, "login.jsp?error=Password");
+        } else {
+            User u = new User();
+            UserBO ubo = new UserBO();
+            u.setUserName(userName);
+            u.setUserPassword(userPassword);
+            if (ubo.authorization(u)) {
+                HttpSession session = request.getSession();
+                session.setAttribute("username", u.getUserName());
+                goPage(request, response, "EmployeeManager");
+            } else {
+                goPage(request, response, "login.jsp?error=Error");
+            }
+        }
+    }
+
+    private void goPage(HttpServletRequest request, HttpServletResponse response, String link)
+            throws ServletException, IOException {
+        RequestDispatcher reqdis = request.getRequestDispatcher(link);
+        reqdis.forward(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
